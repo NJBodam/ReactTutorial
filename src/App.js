aadddb918f42e7917c7f03d48c2cc4d001eb15b3
NJBodam <bodambest@yahoo.com>
2022-01-28 23:15:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import Main from './Main';
+import Navbar from './Navbar';
+
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App;
